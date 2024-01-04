d39c7be286e54ad14758fef11f50890399a89716
Soumyadeep Bera <soumyadeep0230b@gmail.com>
2024-01-04 15:03:23 +0530

    made responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, CSSProperties} from 'react'
+import {ClipLoader, PulseLoader} from "react-spinners";
 import axios from "axios"
+
+const override: CSSProperties = {
+  borderColor: "red",
+};
 
 const App = () => {
   let [movieName, setMovieName] = useState()
+  let [name, setName] = useState()
   let [arr, setArr] = useState([])
   let [see, setSee] = useState(false)
   let [networkError, setNetworkError] = useState(false)
+  let [loading, setLoading] = useState(false)
   
   let url = `https://www.omdbapi.com/?apikey=4768ae48&s=${movieName}`
 
     async function fetchData(e) {
+        setName(movieName)
         e.preventDefault()
         try {
+          setLoading(true)
           let response = await axios.get(url)
           if (response){
           // setMovies(response.data)
@@ -20,9 +29,13 @@ const App = () => {
               console.log("The arr is ",arr)
               setSee(true)
               // return (response.data)
-          } else {
+            } else {
               alert("Error with the API")
-          }
+            }
+            setTimeout(() => {
+              setLoading(false)
+            
+          }, 1500);
 
         } catch(err) {
             setNetworkError(true)
@@ -32,7 +45,6 @@ const App = () => {
   function handleChange(e) {
       setMovieName(e.target.value)
       console.log(e.target.value)
-
   }
   return (
     <>
@@ -42,21 +54,41 @@ const App = () => {
     <div className="navbar-container">
         <div className="navbar-inner-container">
                 <div clasName="navbar-right-container">
-                    LOGO
+                    <h2>MyTube</h2>
                 </div>
                 <div clasName="navbar-left-container">
                   <form onSubmit={fetchData}>
                     <input onChange={handleChange} className="search" type="text" placeholder="Search any movies here"/>
                     
-                    <button type="submit" className="search btn">Search</button>
+                      <button type="submit" className="search btn" style={loading?{backgroundColor: "gray"}: null}>
+                        {loading ? "loading...": "Search"}
+                        </button>
+                    
                     
 
                   </form>
                 </div>
         </div>
-        {see && typeof arr !== "undefined"? <div className="display-movie">
+        {see && typeof arr !== "undefined"? 
+        loading?
+        <div className="loading-container">
+          <ClipLoader
+          loading={loading}
+          color= "#0000"
+          cssOverride={override}
+          size={60}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          />
+          <p>Loading...</p>
+        </div>
+        :
+        <>
+        <div className="result-title">
+          <h2 >Here are the ({arr.length}) results for "{name.toUpperCase()}"</h2>
+        </div>
+        <div className="display-movie">
 
-          {console.log("movie displaying", arr)}
                 {
                     arr.map((index) => {
                         return (
@@ -70,7 +102,8 @@ const App = () => {
                         )
                     })
                 }
-        </div> : 
+        </div>
+        </> : 
         <div>Wrong Input value</div>
         }
     </div>
